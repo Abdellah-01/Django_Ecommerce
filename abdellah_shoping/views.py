3545e0471b50d6c5fb7b85b847ae89628f6d4a7b
Abdellah-01 <abdurrahmansince2001@outlook.com>
2025-08-22 19:12:48 +0530

    Worded With Index HTml Page

--- abdellah_shoping/views.py
+++ abdellah_shoping/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'abdellah_shoping/index.html')
+    header_type = 'header-transparent-bg'
+
+    context = {
+        'header_type':header_type
+    }
+    return render(request, 'abdellah_shoping/index.html', context)
