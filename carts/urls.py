b1d352ee97ed3e7ddfce4abcabb303065c3e0730
Abdellah-01 <abdurrahmansince2001@outlook.com>
2025-08-24 11:34:23 +0530

    Craeted a Logics for Add yo cart in Views.py

--- carts/urls.py
+++ carts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'carts_urls'
 
 urlpatterns = [
-    path('', views.view_cart, name = 'view_cart_page')
+    path('', views.view_cart, name = 'view_cart_page'),
+    path('add_cart/<int:product_id>/', views.add_cart, name = 'add_cart_page')
 ]
