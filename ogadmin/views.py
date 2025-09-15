556eee15dbfb008303f676efb4e8be308879ee48
Abdellah-01 <abdurrahmansince2001@outlook.com>
2025-09-15 20:08:42 +0530

    Worked with Overview

--- ogadmin/views.py
+++ ogadmin/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def overview(request):
-    return HttpResponse('Overview Page')
+    return render(request, 'ogadmin/analytics.html')
