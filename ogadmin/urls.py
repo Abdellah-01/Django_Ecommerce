b7a0220b1932e27e31099b640eb032790ff73b08
Abdellah-01 <abdurrahmansince2001@outlook.com>
2025-09-16 00:00:12 +0530

    Worked With Forget Password Authentication

--- ogadmin/urls.py
+++ ogadmin/urls.py
@@ -7,5 +7,6 @@ app_name = 'ogadmin'
 urlpatterns = [
     path('overview', views.overview, name='overview_admin_page'),
     path('login', views.login, name='login_admin_page'),
+    path('forget_password', views.forget_password, name='forget_password_admin_page'),
     path('logout', views.logout, name='logout_admin_page'),
 ]
--- ogadmin/views.py
+++ ogadmin/views.py
@@ -1,12 +1,22 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
 
 from .forms import AdminLoginForm
+from accounts.models import Account
 
 # Authentication
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
+
+# Email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def login(request):
@@ -30,6 +40,38 @@ def login(request):
         form = AdminLoginForm()
 
     return render(request, 'ogadmin/auth-login.html', {'form': form})
+
+def forget_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        try:
+            user = Account.objects.get(email__exact=email, is_superadmin=True)
+        except Account.DoesNotExist:
+            user = None
+
+        if user:
+            # Send reset email only to superadmin users
+            current_site = get_current_site(request)
+            mail_subject = "Reset Your Password (Super Admin)"
+            message = render_to_string('accounts/reset_pwd_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user)
+            })
+            send_email = EmailMessage(
+                mail_subject,
+                message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[email]
+            )
+            send_email.send()
+            messages.success(request, "Reset email sent successfully!")
+            return redirect('ogadmin:login_admin_page')
+        else:
+            messages.error(request, "Super admin account with this email does not exist!")
+
+    return render(request, 'ogadmin/auth-forget-pwd.html')
 
 @login_required(login_url='ogadmin:login_admin_page')
 def logout(request):
